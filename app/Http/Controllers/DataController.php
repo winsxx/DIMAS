dcfa226aa9f590bca4d3cf7b197400feb2b61174
Winson Waisakurnia <13512071@std.stei.itb.ac.id>
2016-05-08 20:59:45 +0700

    Add query 6 and 7

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -11,15 +11,11 @@ use DB;
 class DataController extends Controller
 {
     public function testDatabase(){
-    	$result = $this->victimMovement("111-56-8948");
-        $result2 = $this->disasterEvent("province", "riau", "banjir", '2012-04-01', '2012-05-01');
-    	return $result2;
-    }
-
-    # Query no 3
-    private function victimMovement($victimId){
-    	$movement = DB::select(DB::raw("SELECT ST_AsGeoJSON(ST_MakeLine(movement.geom)) FROM (SELECT victim_event.geom AS geom FROM victim, victim_event WHERE victim.nik = :victimid AND victim.nik = victim_event.nik ORDER BY victim_event.start_time) AS movement"), array('victimid' => $victimId));
-    	return $movement;
+        $query1 = $this->disasterEvent("province", "riau", "banjir", '2012-04-01', '2012-05-01');
+        $query3 = $this->victimMovement("111-56-8948");
+        $query6 = $this->refugeeCamp("village", "Sawahan");
+        $query7 = $this->medicalFacility("village", "Sawahan");
+    	return $query7;
     }
 
     # Query no 1
@@ -52,5 +48,54 @@ class DataController extends Controller
         $event = DB::select(DB::raw($query));
         return $event;
     }
+
+    # Query no 3
+    private function victimMovement($victimId){
+        $movement = DB::select(DB::raw("SELECT ST_AsGeoJSON(ST_MakeLine(movement.geom)) FROM (SELECT victim_event.geom AS geom FROM victim, victim_event WHERE victim.nik = :victimid AND victim.nik = victim_event.nik ORDER BY victim_event.start_time) AS movement"), array('victimid' => $victimId));
+        return $movement;
+    }
+
+    # Query 6
+    private function refugeeCamp($admLevel, $name){
+        $query = "SELECT refuge_camp.camp_name FROM village, refuge_camp WHERE refuge_camp.location = village.village_id";
+
+        # administration level and name
+        if(isset($admLevel) && isset($name)){
+            if($admLevel == 'village'){
+                $query = "{$query} AND village.village_name='{$name}'";
+            } else if ($admLevel == 'district'){
+                $query = "{$query} AND village.district='{$name}'";
+            } else if ($admLevel == 'subdistrict'){
+                $query = "{$query} AND village.subdistrict='{$name}'";
+            } else if ($admLevel == 'province'){
+                $query = "{$query} AND village.province='{$name}'";
+            }
+        }
+
+        $campList = DB::select(DB::raw($query));
+        return $campList;
+    }
+
+    # Query 7
+    private function medicalFacility($admLevel, $name){
+        $query = "SELECT med_facility.facility_name FROM village, med_facility WHERE med_facility.location = village.village_id";
+
+        # administration level and name
+        if(isset($admLevel) && isset($name)){
+            if($admLevel == 'village'){
+                $query = "{$query} AND village.village_name='{$name}'";
+            } else if ($admLevel == 'district'){
+                $query = "{$query} AND village.district='{$name}'";
+            } else if ($admLevel == 'subdistrict'){
+                $query = "{$query} AND village.subdistrict='{$name}'";
+            } else if ($admLevel == 'province'){
+                $query = "{$query} AND village.province='{$name}'";
+            }
+        }
+
+        $facilityList = DB::select(DB::raw($query));
+        return $facilityList;
+    }
+
 
 }
